742856b57c80ab0ca077f6361b48001aca55ee51
rhonalalonso <rhonal.ac@gmail.com>
2025-05-24 07:31:02 -0500

    Permitir CORS desde frontend en Render

--- catalogo-backend/server.js
+++ catalogo-backend/server.js
@@ -10,8 +10,13 @@ const app = express(); // <-- DEBE estar antes de usar `app`
 app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use(cors({
-  origin: ['http://127.0.0.1:5500', 'http://localhost:5500']
+  origin: [
+    'http://127.0.0.1:5500',
+    'http://localhost:5500',
+    'https://catalogo-decoservi.onrender.com' // ✅ frontend en producción
+  ]
 }));
+
 app.use(express.json());
 
 const rutasProductos = require('./routes/productos');
